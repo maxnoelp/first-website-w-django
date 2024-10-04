935c709c87e33172e6bc890fa556d0607cd36505
Max Noelp <max@surfgreen.dev>
2024-10-04 12:51:25 +0200

    add footer, images in database and models.py

--- shop/models.py
+++ shop/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Kunde(models.Model):
-    benutzer = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    benutzer = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True
+    )
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class Artikel(models.Model):
-    name=models.CharField(max_length=200, null=True)
-    beschreibung=models.TextField(blank=True, null=True)
-    preis=models.FloatField()
+    name = models.CharField(max_length=200, null=True)
+    beschreibung = models.TextField(blank=True, null=True)
+    preis = models.FloatField()
+    bild = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class bestellung(models.Model):
     kunde = models.ForeignKey(Kunde, on_delete=models.SET_NULL, null=True, blank=True)
     bestelldatum = models.DateTimeField(auto_now_add=True)
@@ -26,23 +32,31 @@ class bestellung(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
+
+
 class bestellteArtikel(models.Model):
-    artikel = models.ForeignKey(Artikel, on_delete=models.SET_NULL, null=True, blank=True)
-    Bestellung = models.ForeignKey(bestellung, on_delete=models.SET_NULL, null=True, blank=True)
+    artikel = models.ForeignKey(
+        Artikel, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    Bestellung = models.ForeignKey(
+        bestellung, on_delete=models.SET_NULL, null=True, blank=True
+    )
     menge = models.IntegerField(default=0, null=True, blank=True)
     datum = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.artikel.name
-    
+
+
 class Adresse(models.Model):
     kunde = models.ForeignKey(Kunde, on_delete=models.SET_NULL, null=True, blank=True)
-    Bestellung = models.ForeignKey(bestellung, on_delete=models.SET_NULL, null=True, blank=True)
-    adresse=models.CharField(max_length=200, null=True)
-    plz=models.CharField(max_length=200, null=True)
-    stadt=models.CharField(max_length=200, null=True)
-    land=models.CharField(max_length=200, null=True)
+    Bestellung = models.ForeignKey(
+        bestellung, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    adresse = models.CharField(max_length=200, null=True)
+    plz = models.CharField(max_length=200, null=True)
+    stadt = models.CharField(max_length=200, null=True)
+    land = models.CharField(max_length=200, null=True)
     datum = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
